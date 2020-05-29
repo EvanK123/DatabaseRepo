043b8c826d37330b41e36b2cedc2cd4ba5dedddf
EvanK123 <42948168+EvanK123@users.noreply.github.com>
2020-05-29 14:34:37 -0700

    i have no clue what you guys want me to do

--- src/MusicTable.java
+++ src/MusicTable.java
@@ -1,9 +1,18 @@
-
-public class MusicTable {
-	
-	public static void main()
+public class MusicTable 
+{
+	/** Stores music names, as the piece of code that is being used to access the songs cannot be used with weblinks */
+	public static void Song()
 	{
-		
 	}
-
+	
+	/** Secondary Key is the link, which will probably not be used */
+	public static void Link()
+	{
+	}
+	
+	/** Difficulty is the additional parameter */
+	public static void Difficulty()
+	{
+	}
+	
 }
