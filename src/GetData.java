a0393ed756b54e58b39222bb4606fff1e99c6ea2
EvanK123 <42948168+EvanK123@users.noreply.github.com>
2020-05-29 15:37:14 -0700

    bruh momento

--- src/GetData.java
+++ src/GetData.java
@@ -6,6 +6,12 @@ public class GetData
 	{
 	}
 	
+	/** If score is higher than top, set boolean to true */
+	public static void TestScore()
+	{
+		boolean higher;
+	}
+	
 	/** Query the name for map */
 	public static void QueryName()
 	{
--- src/PutData.java
+++ src/PutData.java
@@ -1,12 +1,6 @@
 
 public class PutData 
 {
-	/** If score is higher than top, set boolean to true */
-	public static void TestScore()
-	{
-		boolean higher;
-	}
-	
 	/** Establish score as #1 */
 	public static void PutScore()
 	{
